f5ba19b6237872b9833606ef5a66e1a9c7903f75
Viktor Dakalov <atlantidu@gmail.com>
2026-05-04 21:33:10 +0500

    Add core modules for intention feature

---
+++ src/core-modules/intention/image.mjs
@@ -0,0 +1,11 @@
+import CoreModule from '../../core-module.mjs';
+
+export default class ImageIntentionCoreModule extends CoreModule {
+  /**
+   *
+   * @param {Core} core
+   */
+  constructor(core) {
+    super(core);
+  }
+}
---
+++ src/core-modules/intention/index.mjs
@@ -0,0 +1,12 @@
+import CoreModule from '../../core-module.mjs';
+
+export default class IntentionCoreModule extends CoreModule {
+  /**
+   *
+   * @param {Core} core
+   */
+  constructor(core) {
+    super(core);
+    
+  }
+}
